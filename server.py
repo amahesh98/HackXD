c781578d6dcf88ae6e643b34bf82368f49a11a3b
Ashwin <mahesh.ashwin1998@gmail.com>
2019-09-21 11:15:28 -0400

    Connected server

--- server.py
+++ server.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, session, jsonify, render_template, redirect, request
 app=Flask(__name__)
 app.secret_key='ChefMate!!'
 
 @app.route('/')
 def renderIndex():
+    session['items']=[]
     return render_template("index.html")
+
+@app.route('/addItem/<item>')
+def addItem(item):
+    items = session['items']
+    items.append(item)
+    session['items']=items
+    return jsonify({'addedItem': item, 'items':items})
+
+@app.route('/processSearch')
+def processSearch():
+    print("Searching")
+    return jsonify({'result': True})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
