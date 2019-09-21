754517ef0dd8fb61f9be8c4e1b0d08a4538e504a
Richa Tenany <richa.tenany@gmail.com>
2019-09-21 03:25:59 -0400

    working so far

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+if($("body").scrollTop()<20){
+    $("#chefmate").append("<b id='temp_container'><p class='line-1 anim-typewriter'>C H E F M A T E <img src='chefman.png'></p></b>");
+}
+var exists = true;
+
 window.smoothScroll = function (target) {
+    
     var scrollContainer = target;
     do { //find scroll container
         scrollContainer = scrollContainer.parentNode;
@@ -20,66 +26,18 @@ window.smoothScroll = function (target) {
     // start scrolling
     scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
 }
-
-window.smoothScroll = function (target) {
-    var scrollContainer = target;
-    do { //find scroll container
-        scrollContainer = scrollContainer.parentNode;
-        if (!scrollContainer) return;
-        scrollContainer.scrollTop += 1;
-    } while (scrollContainer.scrollTop == 0);
-
-    var targetY = 0;
-    do { //find the top of target relatively to the container
-        if (target == scrollContainer) break;
-        targetY += target.offsetTop;
-    } while (target = target.offsetParent);
-
-    scroll = function (c, a, b, i) {
-        i++; if (i > 30) return;
-        c.scrollTop = a + (b - a) / 30 * i;
-        setTimeout(function () { scroll(c, a, b, i); }, 20);
+$("body").scroll(function(){
+    if($("body").scrollTop()>=20){
+        $("#temp_container").remove();
+        exists = false;
+    } 
+    else if($("body").scrollTop()<20 && exists==false){
+        $("#chefmate").append("<b id='temp_container'><p class='line-1 anim-typewriter'>C H E F M A T E <img src='chefman.png'></p></b>");
     }
-    // start scrolling
-    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
-}
+})
 
-var ingredients = [
-    "romaine lettuce",
-      "Black Olives", "Grape Tomatoes","Garlic","Pepper",
-      "Purple Onion",
-      "Seasoning",
-      "Garbanzo Beans",
-      "Feta Cheese Crumbles",
-      "Plain Flour",
-      "Ground Pepper",
-      "Salt",
-      "Tomatoes",
-      "Ground Black Pepper",
-      "Thyme",
-      "Eggs",
-      "Green Tomatoes",
-      "Yellow Corn Meal",
-      "Milk",
-      "Vegetable Oil", "Chicken Broth",
-      "Parsley",
-      "Dried Basil",
-      "Garlic",
-      "Bone in Chicken Thighs",
-      "Pasta",
-      "Olive Oil",
-      "Crushed Red Pepper",
-      "Crushed Tomatoes",
-      "Diced Tomatoes",
-      "All-Purpose Flour",
-      "Pepper",
-      "Butter",
-      "Dry white wine",
-      "White wine vinegar",
-      "Lemon zest",
-      "Fresh tarragon",
-      "Shallots"
-  ];
+var ingredients = 'ingredientsList.txt'
+
   function getMatches()
   {
     var query = document.getElementById('myInput').value;
